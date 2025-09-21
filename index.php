617e3bb1c51e632e329acf495271d1e0dbff6986
Caio Cleim <caiocleim54@gmail.com>
2025-09-21 19:39:42 -0300

    mudando telas front

--- index.php
+++ index.php
@@ -22,6 +22,96 @@
            <div class="items-bar">
                 <div class="item">
                     <a href="#">Categorias</a>
+
+                   <div class="box-item">
+                            
+                            <ul>
+                                <h1>Construção e Reformas</h1>
+                                <a href="#">Pedreiro</a>
+                                <a href="#">Pintor</a>
+                                <a href="#">Eletricista</a>
+                                <a href="#">Encanador</a>
+                                <a href="#">Gesseiro</a>
+                                <a href="#">Serralheiro</a>
+                                <a href="#">Marceneiro</a>
+                                <a href="#">Azuleijista</a>
+
+                            </ul>
+                           
+                            <ul>
+                                <h1>Manuntenção e Reparos</h1>
+                                <a href="#">Técnico de Informatica</a>
+                                <a href="#">Manuntenção de Eletrodomesticos</a>
+                                <a href="#">Mecânico de Autos e Motos</a>
+                                <a href="#">Chaveiro</a>
+                                <a href="#">Vidraceiro</a>
+                                <a href="#">Instalador de Ar-Condicionado</a>
+                            </ul>
+                            <ul>
+                                <h1>Limpeza e Conservação</h1>
+                                <a href="#">Diarista</a>
+                                <a href="#">Faxineiro(a)</a>
+                                <a href="#">Jardinagem</a>
+                                <a href="#">Detetização</a>
+                                <a href="#">Lavagem de Sofás</a>
+                                <a href="#">Piscineiro</a>
+                            </ul>
+                            <ul>
+                                <h1>Consultoria e Serviços Profissionais</h1>
+                                <a href="#">Contador</a>
+                                <a href="#">Advogado</a>
+                                <a href="#">Designer Gráfico</a>
+                                <a href="#">Marketing Digital</a>
+                                <a href="#">Consultoria Empresarial</a>
+                                <a href="#">Desenvolvedor de Sites / Apps</a>
+                            </ul>
+
+                            <ul>
+                                <h1>Saúde e Bem-estar</h1>
+                                <a href="#">Personal Trainer</a>
+                                <a href="#">Massoterapeuta</a>
+                                <a href="#">Cabeleireiro</a>
+                                <a href="#">Esteticista</a>
+                                <a href="#">Manicure</a>
+                                <a href="#">Nutricionista</a>
+                                <a href="#">Psicólogo</a>
+                            </ul>
+
+                            <ul>
+                                <h1>Educação e Treinamentos</h1>
+                                <a href="#">Professor Particular</a>
+                                <a href="#">Aulas de Dança</a>
+                                <a href="#">Aulas de Informatica</a>
+                                <a href="#">Cursos Profissionalizantes</a>
+                            </ul>
+                            <ul>
+                                <h1>Eventos e Entretenimento</h1>
+                                <a href="#">Fotógrafo</a>
+                                <a href="#">Filmagem</a>
+                                <a href="#">DJ</a>
+                                <a href="#">Banda/Músicos</a>
+                                <a href="#">Cerimonialista</a>
+                                <a href="#">Decoração de Festas</a>
+                                <a href="#">Buffet</a>
+                                <a href="#">Locação de Equipamentos</a>
+                            </ul>
+                            <ul>
+                                <h1>Transportes e Logística</h1>
+                                <a href="#">Motorista Particular</a>
+                                <a href="#">Frete / Carretos</a>
+                                <a href="#">Mudanças</a>
+                                <a href="#">Motoboy / Entregador</a>
+                            </ul>
+                            <ul>
+                                <h1>Serviços Domésticos e Pessoais</h1>
+                                <a href="#">Babá</a>
+                                <a href="#">Cuidador de Idosos</a>
+                                <a href="#">Passeador de Cães</a>
+                                <a href="#">Adestrador de Animais</a>
+                                <a href="#">Costureira</a>
+                            
+                            </ul>
+                    </div> 
                 </div>
                 <div class="item">
                     <a href="#">Torne-se um prestador</a>
@@ -33,13 +123,28 @@
         </div>
 
         <div class="user-area">
-            
+        
+            <img src="public/images/default-user.jpg" alt="">
+
+            <a style="font-weight:700;" href="#">Olá, Usuário!</a>
+
+
         </div>
     
     </header>
+
+
+
+
+
     <main>
 
     </main>
-    <footer></footer>
+    <footer>
+
+        &copy Todos os Direitos Reservados - 2025
+
+
+    </footer>
 </body>
 </html>
