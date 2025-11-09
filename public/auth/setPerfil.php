13d7b096c86bb1a64f99bb117bc721bf5cb9970e
Victor <victorhenriquesan@outlook.com>
2025-11-09 00:43:34 -0300

    Correcao Criar Comerciante

--- public/auth/setPerfil.php
+++ public/auth/setPerfil.php
@@ -4,14 +4,10 @@ session_start();
 if (isset($_POST['perfil'])) {
     $perfil = $_POST['perfil'];
 
-    // 1. Salva a escolha na sessão
     $_SESSION['perfil'] = $perfil;
-
-    // 2. Redireciona para a página do formulário de cadastro
     header("Location: ../pages/auth/registrar.php"); 
     exit();
 } else {
-    // Redireciona de volta se a requisição não for válida
     header("Location: ../pages/auth/cliente_comerciante.php");
     exit();
 }
